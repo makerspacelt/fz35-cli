485cb0f2ba121c71d7c4c41d0fde7350b31412cb
darius <darius@vozbutas.lt>
2019-01-20 20:42:08 +0200

    adding full-sweep test example

---
+++ full_range_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+./load.sh off slp 0A slp getCsvRow on slp \
+	0.05A slp getCsvRow \
+	0.10A slp getCsvRow \
+	0.15A slp getCsvRow \
+	0.20A slp getCsvRow \
+	0.25A slp getCsvRow \
+	0.30A slp getCsvRow \
+	0.35A slp getCsvRow \
+	0.40A slp getCsvRow \
+	0.45A slp getCsvRow \
+	0.5A slp getCsvRow \
+	0.6A slp getCsvRow \
+	0.7A slp getCsvRow \
+	0.8A slp getCsvRow \
+	0.9A slp getCsvRow \
+	1A slp getCsvRow \
+	1.1A slp getCsvRow \
+	1.2A slp getCsvRow \
+	1.3A slp getCsvRow \
+	1.4A slp getCsvRow \
+	1.5A slp getCsvRow \
+	1.6A slp getCsvRow \
+	1.7A slp getCsvRow \
+	1.8A slp getCsvRow \
+	1.9A slp getCsvRow \
+	2A slp getCsvRow \
+	2.2A slp getCsvRow \
+	2.4A slp getCsvRow \
+	2.6A slp getCsvRow \
+	2.8A slp getCsvRow \
+	3A slp getCsvRow \
+	3.2A slp getCsvRow \
+	3.4A slp getCsvRow \
+	3.6A slp getCsvRow \
+	3.8A slp getCsvRow \
+	4A slp getCsvRow \
+	4.2A slp getCsvRow \
+	4.4A slp getCsvRow \
+	4.6A slp getCsvRow \
+	4.8A slp getCsvRow \
+	5A slp getCsvRow \
+	off slp 0A
