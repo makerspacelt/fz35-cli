77870a035f1d44fa05f7ed9f16adaf4d4ff534f1
darius <darius@vozbutas.lt>
2019-05-28 23:44:07 +0300

    adding few more samples

---
+++ sample_tests/fine_test_0-2a.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+./load.sh off slp setup 0A slp getCsvRow on slp \
+	0.01A slp getCsvRow \
+	0.02A slp getCsvRow \
+	0.03A slp getCsvRow \
+	0.04A slp getCsvRow \
+	0.05A slp getCsvRow \
+	0.06A slp getCsvRow \
+	0.07A slp getCsvRow \
+	0.08A slp getCsvRow \
+	0.09A slp getCsvRow \
+	0.10A slp getCsvRow \
+	0.12A slp getCsvRow \
+	0.14A slp getCsvRow \
+	0.16A slp getCsvRow \
+	0.18A slp getCsvRow \
+	0.20A slp getCsvRow \
+	0.22A slp getCsvRow \
+	0.24A slp getCsvRow \
+	0.26A slp getCsvRow \
+	0.28A slp getCsvRow \
+	0.30A slp getCsvRow \
+	0.32A slp getCsvRow \
+	0.34A slp getCsvRow \
+	0.36A slp getCsvRow \
+	0.38A slp getCsvRow \
+	0.40A slp getCsvRow \
+	0.42A slp getCsvRow \
+	0.44A slp getCsvRow \
+	0.46A slp getCsvRow \
+	0.48A slp getCsvRow \
+	0.50A slp getCsvRow \
+	0.55A slp getCsvRow \
+	0.60A slp getCsvRow \
+	0.65A slp getCsvRow \
+	0.70A slp getCsvRow \
+	0.75A slp getCsvRow \
+	0.80A slp getCsvRow \
+	0.85A slp getCsvRow \
+	0.90A slp getCsvRow \
+	0.95A slp getCsvRow \
+	1A slp getCsvRow \
+	1.05A slp getCsvRow \
+	1.10A slp getCsvRow \
+	1.15A slp getCsvRow \
+	1.20A slp getCsvRow \
+	1.25A slp getCsvRow \
+	1.30A slp getCsvRow \
+	1.35A slp getCsvRow \
+	1.40A slp getCsvRow \
+	1.45A slp getCsvRow \
+	1.50A slp getCsvRow \
+	1.55A slp getCsvRow \
+	1.60A slp getCsvRow \
+	1.65A slp getCsvRow \
+	1.70A slp getCsvRow \
+	1.75A slp getCsvRow \
+	1.80A slp getCsvRow \
+	1.85A slp getCsvRow \
+	1.90A slp getCsvRow \
+	1.95A slp getCsvRow \
+	2A slp getCsvRow \
+	off slp 0A
+
